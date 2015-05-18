2423e4401cfb581fdd1535759c260d0420d88bf4
bhupinder <bhupinder@logicerp.com>
2015-05-18 18:17:13 +0530

    worked on the filter section

--- app/scripts/controllers/getdata.js
+++ app/scripts/controllers/getdata.js
@@ -8,13 +8,14 @@
  * Controller of the accentsApp
  */
 angular.module('accentsApp')
-  .controller('getdataCtrl', function ($scope,getRecords) {
+  .controller('getdataCtrl', function ($scope,$http,getRecords) {
   $scope.docs={};
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
 	
 	// $scope.getAllData = function() {
       getRecords.getAllData()
@@ -22,7 +23,7 @@ angular.module('accentsApp')
 			
 		if(data.rows)
 		{
-		console.log(data.rows);
+		//console.log(data.rows.doc);
 		
 	$scope.docs=data.rows;
 	$scope.count=data.total_rows;
@@ -34,7 +35,7 @@ angular.module('accentsApp')
           
         })
         .error(function(error) {
-        console.log(error);
+      //  console.log(error);
         });
         
         $scope.deletedoc = function(id) {
@@ -45,4 +46,41 @@ angular.module('accentsApp')
         
         
     };
+    
+    
+    
+    $scope.adddata=function(){
+    alert('hi');
+   /* $http.post({
+        url: 'http://diacritics.iriscouch.com/accents_swarandeep/',
+        
+        data: JSON.stringify({"_rev": "4-0795f5da8974dc19e1178a0fd11d890b","id":"4cf643637","source": "marciel",   "original": "شرح الفوائد", "definition": "by Shaykh Aḥmad-i-Aḥsá’í", "type": "term", "user": "chad",   "term": "_Sharḥu’l-Far"}),
+        headers: {'Content-Type': 'application/json'}
+      }).success(function (data, status, headers, config) {
+           alert(data);
+        }).error(function (data, status, headers, config) {
+            $scope.status = status + ' ' + headers;
+        });*/
+};  
+    
+    
+    
+    $scope.getnames=function(searchval){
+		$http.get('http://diacritics.iriscouch.com/accents_swarandeep/_design/lists/_view/full_term?startkey="'+searchval+'"&endkey="'+searchval+'\\ufff0"&include_docs=true')
+		.success(function(data) {
+			console.log(data);
+		if(data.rows)
+		{
+	
+	$scope.docs=data.rows;
+	$scope.count=data.total_rows;
+		}
+           })
+        .error(function(error) {
+        
+        });
+        
+	}
+    
+    
   });
